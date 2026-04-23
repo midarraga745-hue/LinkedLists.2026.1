1167d8795d78c147c4c7f5ede6e4e8f04a3f3d22
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-04-23 16:05:04 -0500

    Simple Linked List

---
+++ LinkedLists/SimpleList/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleList
+{
+    public class Node<T>
+    {
+        public Node(T? data)
+        {
+            Data = data;
+        }
+
+        public object Data { get; internal set; }
+        public Node<T> Next { get; internal set; }
+    }
+}
